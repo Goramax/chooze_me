2a52f8d4ffcabe90a09140fc3628b51e03a1796f
Goramax <maxime.malherbe@ecole404.fr>
2023-10-17 16:41:57 +0200

    swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Chooze me')
+    .setDescription("Documentation de l'api Chooze me")
+    .setVersion('1.0')
+    .addTag('ChoozeMe')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
